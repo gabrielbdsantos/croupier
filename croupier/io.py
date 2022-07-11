44799b0644050bcd57d1659365f34122a5ccbecb
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2022-07-11 16:24:13 -0300

    feat(io): export trajectories to a single file

--- croupier/io.py
+++ croupier/io.py
@@ -8,14 +8,13 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def export_design(
+def export_design_to_directory(
     desing_matrix: NDArray[np.floating],
-    path: PathLike,
-    single_file: bool = True,
+    output_dir: PathLike,
     file_prefix: str = "trajectory",
     fmt: str = "%.6f",
 ) -> None:
-    """Export a set of trajectories to individual files.
+    """Export a set of trajectories to individual files in a directory.
 
     Parameters
     ----------
@@ -24,16 +23,16 @@ def export_design(
         three-dimensional matrix in which the first dimension represents
         the trajectories. The second and third dimensions represent the
         number of points and number of parameters in each trajectory.
-    path : path-like
-        Path to the output file or folder.
+    output_dir : path-like
+        Path to the output directory.
     single_file: bool, optional
         Choose whether to output all trajectories to a single file or to
         write each trajectory to a different files in the specified
         folder.
     file_prefix : str, optional
-        The string used as a prefix for the each trajectory. It is used
-        only if `single_file` is False. Each trajectory is written on a
-        separe file, which follows the following naming convention:
+        The string used as a prefix for the each trajectory. Each
+        trajectory is written on a separe file, which follows the
+        following naming convention:
             {output folder}/{name prefix}_{trajectory number}.txt
     fmt : str, optional
         A single format, a sequence of formats, or a multi-format
@@ -52,16 +51,17 @@ def export_design(
         Expected a three-dimensional design matrix.
     """
     # Put the output folder name in terms of absolute path.
-    path = pathlib.Path(path)
+    path = pathlib.Path(output_dir)
 
-    if not single_file:
-        # If the output folder does not exist, try to create it. It may happen
-        # that the user does not have permission to write to the output folder.
-        # If this does happen, then throw a PermissionError.
-        path.parent.mkdir(parents=True, exist_ok=False)
+    # If the output folder does not exist, try to create it. It may
+    # happen that the user does not have permission to write to the
+    # output folder. If this does happen, then throw a PermissionError.
+    # If the folder exists, throw a FileExistsError.
+    path.mkdir(parents=True, exist_ok=False)
 
     # Total number of trajectories
     num_trajectories = desing_matrix.shape[0]
+    num_trajectories_digits = int(np.ceil(np.log10(num_trajectories)) + 1.0)
 
     # If the number of dimensions of the trajectory matrix is different from
     # three, throw an ValueError.
@@ -72,20 +72,47 @@ def export_design(
         )
 
     for i, trajectory in enumerate(desing_matrix):
-        output = (
-            path
-            if single_file
-            else (
-                path / f"{file_prefix}_{i:0{num_trajectories // 10 + 1}}.txt"
-            )
-        )
-
-        kws = (
-            {"header": f"Trajectory {i}", "footer": "--"}
-            if single_file
-            else {}
-        )
+        # Set the output format.
+        output = path / f"{file_prefix}_{i:0{num_trajectories_digits}}.txt"
 
         # Write the trajectory to file.
-        with open(output, "w+") as file:
-            np.savetxt(file, trajectory, fmt=fmt, **kws)
+        np.savetxt(output, trajectory, fmt=fmt)
+
+
+def export_design_to_file(
+    desing_matrix: NDArray[np.floating],
+    output_file: PathLike,
+    fmt: str = "%.6f",
+) -> None:
+    """Export a set of trajectories to individual files in a directory."""
+    # Put the output folder name in terms of absolute path.
+    path = pathlib.Path(output_file)
+
+    # If the output file does not exist, try to create it. It may
+    # happen that the user does not have permission to write to the
+    # output folder. If this does happen, then throw a PermissionError.
+    # If the file exists, throw a FileExistsError.
+    path.touch(exist_ok=False)
+
+    # Total number of trajectories
+    num_trajectories = desing_matrix.shape[0]
+    num_trajectories_digits = int(np.ceil(np.log10(num_trajectories)) + 1.0)
+
+    # If the number of dimensions of the trajectory matrix is different from
+    # three, throw an ValueError.
+    if desing_matrix.ndim != 3:
+        raise ValueError(
+            "Expected a three-dimensional trajectory matrix."
+            f" Found a matrix with {desing_matrix.ndim}"
+        )
+
+    with open(path, "w+") as file:
+        for i, trajectory in enumerate(desing_matrix):
+            # Write the trajectory to file.
+            np.savetxt(
+                file,
+                trajectory,
+                fmt=fmt,
+                header=f"Trajectory {i:0{num_trajectories_digits}}",
+                footer="--",
+            )
