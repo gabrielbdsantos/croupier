548a041900145b9bb249ca3fadb7ea40ba75ad4e
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2023-08-12 19:20:28 -0300

    fix: correct typo

--- croupier/io.py
+++ croupier/io.py
@@ -9,7 +9,7 @@ from numpy.typing import NDArray
 
 
 def export_design_to_directory(
-    desing_matrix: NDArray[np.floating],
+    design_matrix: NDArray[np.floating],
     output_dir: PathLike,
     file_prefix: str = "trajectory",
     fmt: str = "%.6f",
@@ -19,17 +19,13 @@ def export_design_to_directory(
 
     Parameters
     ----------
-    trajectory : np.ndarray
+    design_matrix : np.ndarray
         The set of trajectories to be exported. It must be a
         three-dimensional matrix in which the first dimension represents
         the trajectories. The second and third dimensions represent the
         number of points and number of parameters in each trajectory.
     output_dir : path-like
         Path to the output directory.
-    single_file: bool, optional
-        Choose whether to output all trajectories to a single file or to
-        write each trajectory to a different files in the specified
-        folder.
     file_prefix : str, optional
         The string used as a prefix for the each trajectory. Each
         trajectory is written on a separe file, which follows the
@@ -64,18 +60,18 @@ def export_design_to_directory(
     path.mkdir(parents=True, exist_ok=overwrite)
 
     # Total number of trajectories
-    num_trajectories = desing_matrix.shape[0]
+    num_trajectories = design_matrix.shape[0]
     num_trajectories_digits = int(np.ceil(np.log10(num_trajectories)) + 1.0)
 
     # If the number of dimensions of the trajectory matrix is different from
     # three, throw an ValueError.
-    if desing_matrix.ndim != 3:
+    if design_matrix.ndim != 3:
         raise ValueError(
             "Expected a three-dimensional trajectory matrix."
-            f" Found a matrix with {desing_matrix.ndim}"
+            f" Found a matrix with {design_matrix.ndim}"
         )
 
-    for i, trajectory in enumerate(desing_matrix):
+    for i, trajectory in enumerate(design_matrix):
         # Set the output format.
         output = path / f"{file_prefix}_{i:0{num_trajectories_digits}}.txt"
 
